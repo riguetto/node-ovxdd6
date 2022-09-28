1085648cbae9db37b2373af80d3ba06a16f7971c
riguetto <108355115+riguetto@users.noreply.github.com>
2022-09-28 15:38:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ovxdd6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ovxdd6)
---
+++ index.js
@@ -0,0 +1,24 @@
+class programmer {
+constructor(nome,nascimento, casado, linguagensfavoritas = [], trabalhodesde = null){
+this.nome = nome 
+this.nascimento = nascimento  
+this.casado = casado
+this.linguagemfavorita = linguagemfavorita 
+this.trabalhodesde = trabalhodesde
+
+}
+}
+const leonardo = new programmer('leonardo andre', '27/06/2005', true ,['javascript','php' ,'python'] 2022);
+
+const isabele = new programmer('isabele diniz', '10/09/2004', true ,['ruby','dart' ,'python' ] 2022);
+ 
+
+
+
+// toda linguagem tem 
+//uma boa comunicação necessita de uma boa sintaxe
+//82% dos erros dos iniciantes e por conta de sintaxe 
+
+´´´js 
+console.log('bem vindos ao video do leo')
+´´´ 
